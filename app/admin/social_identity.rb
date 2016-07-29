1f42f08de1013d53be63ece67ac26927eb3a4368
ftopico <fredericktopico@gmail.com>
2016-07-29 16:41:15 +0800

    Added ActiveAdmin models

--- app/admin/social_identity.rb
+++ app/admin/social_identity.rb
@@ -1,10 +1,9 @@
-ActiveAdmin.register User do
+ActiveAdmin.register SocialIdentity do
 
-  menu priority: 1
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :first_name, :last_name, :email, :encrypted_password, :is_active
+  permit_params :user_id, :uid, :attributes, :provider, :raw_object
 #
 # or
 #
